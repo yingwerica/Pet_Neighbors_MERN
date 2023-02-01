b04fffddeb6e99afa1ead94f991c52f68bc16453
yingwerica <yingw.pro@gmail.com>
2023-02-01 16:01:27 -0700

    modify to test deploy for frontend

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -2,19 +2,6 @@ import React from 'react'
 
 export const Login = () => {
   return (
-    <Form.Group><Form.Control
-            name="username"
-            placeholder="Email Address/Username"
-            style={{ marginBottom: "1rem" }}
-         
-          />
-          <Form.Control
-            name="password"
-           
-            placeholder="Password"
-            style={{ marginBottom: "1rem" }}
-            type="password"
-          />
-        </Form.Group>
+   <h1>Login</h1>
   )
 }
